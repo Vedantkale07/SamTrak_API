0e1c7863486f0ee1694de9e3f74ec3192dbcbdb1
vedant Kale <vedantkale0707@gmail.com>
2024-10-03 23:37:15 +0530

    add Angular frontend templete Demo

---
+++ TempleteEditing/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+const routes: Routes = [
+  {
+    path:'', component:HomeComponent
+  },
+  {
+    path:'login', component:LoginComponent
+  },
+
+  {
+    path:'registration', component:RegistrationComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
